da144f9bd714ac52ae4ac42f872db533bb41ab61
Tim Schmelmer <tim.schmelmer@gmail.com>
2023-06-29 16:49:20 -0600

    adding linkedin info and making a call to GH gists

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,7 @@
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from third_parties.linkedin import scrape_linkedin_profile
 
 
 def main(inf):
@@ -21,32 +22,35 @@ def main(inf):
 
 
 if __name__ == "__main__":
-    info = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. He is the 
-    founder, CEO, and chief engineer of SpaceX; angel investor, CEO, and product architect of Tesla, Inc.; owner, 
-    CTO, and chairman of Twitter; founder of the Boring Company and X Corp.; co-founder of Neuralink and OpenAI; and 
-    president of the philanthropic Musk Foundation. Musk is the wealthiest person in the world according to the 
-    Bloomberg Billionaires Index and Forbes's Real Time Billionaires list as of June 2023, primarily from his 
-    ownership stakes in Tesla and SpaceX, with an estimated net worth of around $225 billion according to Bloomberg 
-    and $235 billion according to Forbes.[4][5][6]
+#     info = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. He is the
+#     founder, CEO, and chief engineer of SpaceX; angel investor, CEO, and product architect of Tesla, Inc.; owner,
+#     CTO, and chairman of Twitter; founder of the Boring Company and X Corp.; co-founder of Neuralink and OpenAI; and
+#     president of the philanthropic Musk Foundation. Musk is the wealthiest person in the world according to the
+#     Bloomberg Billionaires Index and Forbes's Real Time Billionaires list as of June 2023, primarily from his
+#     ownership stakes in Tesla and SpaceX, with an estimated net worth of around $225 billion according to Bloomberg
+#     and $235 billion according to Forbes.[4][5][6]
+#
+# Musk was born in Pretoria, South Africa, and briefly attended the University of Pretoria before moving to Canada at
+# age 18, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at Queen's
+# University and transferred to the University of Pennsylvania, where he received bachelor's degrees in economics and
+# physics. He moved to California in 1995 to attend Stanford University. After two days, he dropped out and,
+# with his brother Kimbal, co-founded the online city guide software company Zip2. In 1999, Zip2 was acquired by Compaq
+# for $307 million and Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal,
+# which eBay acquired for $1.5 billion in 2002.
+#
+# With $175.8 million, Musk founded SpaceX in 2002, a spaceflight services company. In 2004, he was an early investor
+# in the electric vehicle manufacturer Tesla Motors, Inc. (now Tesla, Inc.). He became its chairman and product
+# architect, assuming the position of CEO in 2008. In 2006, he helped create SolarCity, a solar energy company that was
+# later acquired by Tesla and became Tesla Energy. In 2015, he co-founded OpenAI, a nonprofit artificial intelligence
+# research company. The following year, he co-founded Neuralink—a neurotechnology company developing brain–computer
+# interfaces—and the Boring Company, a tunnel construction company. Musk has also proposed a hyperloop high-speed
+# vactrain transportation system. In 2022, his acquisition of Twitter for $44 billion was completed.
+#
+# Musk has expressed views that have made him a polarizing figure. He has been criticized for making unscientific and
+# misleading statements, including that of spreading COVID-19 misinformation. In 2018, the U.S. Securities and Exchange
+# Commission (SEC) sued Musk for falsely tweeting that he had secured funding for a private takeover of Tesla. Musk
+# stepped down as chairman of Tesla and paid a $20 million fine as part of a settlement agreement with the SEC."""
 
-Musk was born in Pretoria, South Africa, and briefly attended the University of Pretoria before moving to Canada at 
-age 18, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at Queen's 
-University and transferred to the University of Pennsylvania, where he received bachelor's degrees in economics and 
-physics. He moved to California in 1995 to attend Stanford University. After two days, he dropped out and, 
-with his brother Kimbal, co-founded the online city guide software company Zip2. In 1999, Zip2 was acquired by Compaq 
-for $307 million and Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal, 
-which eBay acquired for $1.5 billion in 2002.
 
-With $175.8 million, Musk founded SpaceX in 2002, a spaceflight services company. In 2004, he was an early investor 
-in the electric vehicle manufacturer Tesla Motors, Inc. (now Tesla, Inc.). He became its chairman and product 
-architect, assuming the position of CEO in 2008. In 2006, he helped create SolarCity, a solar energy company that was 
-later acquired by Tesla and became Tesla Energy. In 2015, he co-founded OpenAI, a nonprofit artificial intelligence 
-research company. The following year, he co-founded Neuralink—a neurotechnology company developing brain–computer 
-interfaces—and the Boring Company, a tunnel construction company. Musk has also proposed a hyperloop high-speed 
-vactrain transportation system. In 2022, his acquisition of Twitter for $44 billion was completed.
-
-Musk has expressed views that have made him a polarizing figure. He has been criticized for making unscientific and 
-misleading statements, including that of spreading COVID-19 misinformation. In 2018, the U.S. Securities and Exchange 
-Commission (SEC) sued Musk for falsely tweeting that he had secured funding for a private takeover of Tesla. Musk 
-stepped down as chairman of Tesla and paid a $20 million fine as part of a settlement agreement with the SEC."""
-    main(info)
+    linkedin_data = scrape_linkedin_profile("https://www.linkedin.com/in/timschmelmer/")
+    main(linkedin_data)
---
+++ third_parties/linkedin.py
@@ -0,0 +1,27 @@
+import os
+import requests
+
+def scrape_linkedin_profile(linkedin_profile_url: str):
+    """Scrape information from a LinkedIn profile,
+    Manually scrape the information from a LinkedIn profile."""
+#    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    api_endpoint = "https://gist.githubusercontent.com/timbogit/9dee5134cc6c543731525e1f54683cef/raw" \
+                   "/2cea675cafb3f0723b77b73f78ec529172a70532/linkedin_tim.json"
+    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+
+    response = requests.get(
+        api_endpoint, params={"url": linkedin_profile_url}#, headers=header_dic
+    )
+
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications", "experiences"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
