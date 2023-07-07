96893ff8213f287b0f71011bf5e2697bebd0ab34
Tim Schmelmer <tim.schmelmer@gmail.com>
2023-07-07 16:44:55 -0600

    Adding output parsing

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,17 +6,16 @@ from third_parties.twitter import scrape_user_tweets
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
 
 
-def main(inf):
-    print("Hello LangChain!")
-
-    linkedin_profile_url = linkedin_lookup_agent(name=inf)
+def ice_break(name: str) -> PersonIntel:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url=linkedin_profile_url, fake=True
     )
 
-    twitter_username = twitter_lookup_agent(name=inf)
+    twitter_username = twitter_lookup_agent(name=name)
     tweets = scrape_user_tweets(username=twitter_username, num_tweets=5)
 
     summary_template = """
@@ -24,18 +23,26 @@ def main(inf):
          1. a short summary
          2. two interesting facts about them
          3. A topic that may interest them
-         4. 2 creative Ice breakers to open a conversation with them 
+         4. 2 creative Ice breakers to open a conversation with them
+                  \n{format_instructions}
      """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+
+    return person_intel_parser.parse(result)
 
 
 if __name__ == "__main__":
-    main("Tim Schmelmer Salesforce")
+    print("Hello LangChain!")
+    result = ice_break(name="Tim Schmelmer")
+    print(result)
